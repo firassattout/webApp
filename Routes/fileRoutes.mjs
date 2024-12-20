d66ebb1ec3afa8ef2d5d0c1f1060ddd19973d528
firassattout <firas.sattout@gmail.com>
2024-12-20 22:21:59 +0300

    fix

--- Routes/fileRoutes.mjs
+++ Routes/fileRoutes.mjs
@@ -2,7 +2,7 @@ import express from "express";
 
 import { checkUser } from "../middleware/checkUser.mjs";
 import multer from "multer";
-import { createFile, showFiles } from "../controllers/FileController.mjs";
+import { createFile, showFiles } from "../controllers/fileController.mjs";
 const upload = multer();
 export const fileRoutes = express.Router();
 
